4e8bf833cd2f256e570d13d8466e72e42203de45
Karake Omar <anzymen@gmail.com>
2019-07-31 18:11:59 +0200

    create index.php

---
+++ index.php
@@ -0,0 +1,124 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!--
+    /   Multipurpose: Free Template by FreeHTML5.co
+    /   Author: https://freehtml5.co
+    /   Facebook: https://facebook.com/fh5co
+    /   Twitter: https://twitter.com/fh5co
+    -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Document title -->
+    <title>Hosting system</title>
+    <!-- Stylesheets & Fonts -->
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,700i%7CRajdhani:400,600,700"
+        rel="stylesheet">
+    <!-- Plugins Stylesheets -->
+    <link rel="stylesheet" href="assets/css/loader/loaders.css">
+    <link rel="stylesheet" href="assets/css/font-awesome/font-awesome.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/aos/aos.css">
+    <link rel="stylesheet" href="assets/css/swiper/swiper.css">
+    <link rel="stylesheet" href="assets/css/lightgallery.min.css">
+    <!-- Template Stylesheet -->
+    <link rel="stylesheet" href="assets/css/style.css">
+    <!-- Responsive Stylesheet -->
+    <link rel="stylesheet" href="assets/css/responsive.css">
+</head>
+
+<body>
+    <!-- Loader Start -->
+    <div class="css-loader">
+        <div class="loader-inner line-scale d-flex align-items-center justify-content-center"></div>
+    </div>
+    <!-- Loader End -->
+    <!-- Header Start -->
+    <header class="position-absolute w-100">
+        <?php
+		include'navigation.php';
+		?>
+    </header>
+    <!-- Header End -->
+    <!-- Hero Start -->
+    <section class="hero">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 offset-md-1 col-md-11">
+                    <div class="swiper-container hero-slider">
+                        <div class="swiper-wrapper">
+                            <div class="swiper-slide slide-content d-flex align-items-center">
+                                <div class="single-slide">
+                                    <h1 data-aos="fade-right" data-aos-delay="200">buy<br> domain and hosting
+                                    </h1>
+                                    <p data-aos="fade-right" data-aos-delay="600">Here you can buy the best domain and hosting
+									at low price.
+                                    </p>
+                                    <a data-aos="fade-right" data-aos-delay="900" href="hostings.php" class="btn btn-primary">See
+                                        More</a>
+                                    <a data-aos="fade-right" data-aos-delay="900" href="login.php" class="btn btn-primary">Get
+                                        Now</a>
+                                </div>
+                            </div>
+                            <div class="swiper-slide slide-content d-flex align-items-center">
+                                <div class="single-slide">
+                                    <h1 data-aos="fade-right" data-aos-delay="200">buy<br> domain and hosting
+                                    </h1>
+                                    <p data-aos="fade-right" data-aos-delay="600">the best way to a good future
+                                    </p>
+                                    <a data-aos="fade-right" data-aos-delay="900" href="hostings.php" class="btn btn-primary">See
+                                        More</a>
+                                    <a data-aos="fade-right" data-aos-delay="900" href="login.php" class="btn btn-primary">Get
+                                        Now</a>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            <!-- Add Control -->
+            <span class="arr-left"><i class="fa fa-angle-left"></i></span>
+            <span class="arr-right"><i class="fa fa-angle-right"></i></span>
+        </div>
+        <div class="texture"></div>
+        <div class="diag-bg"></div>
+    </section>
+    <!-- Hero End -->
+    <!-- Call To Action Start -->
+    <!-- Call To Action End -->
+    <!-- Services Start -->
+    <!-- Services End -->
+    <!-- Featured Start -->
+    <!-- Featured End -->
+    <!-- Recent Posts Start -->
+    <!-- Recent Posts End -->
+    <!-- Trust Start -->
+    <!-- Trust End -->
+    <!-- Pricing Start -->
+    <!-- Pricing End -->
+    <!-- Testimonial and Clients Start -->
+    <!-- Testimonial and Clients End -->
+    <!-- Call To Action 2 Start -->
+    <!-- Call To Action 2 End -->
+    <!-- Footer Start -->
+    <?php
+	include'footer.php';
+	?>
+    <!-- Footer Endt -->
+    <!--jQuery-->
+    <script src="assets/js/jquery-3.3.1.js"></script>
+    <!--Plugins-->
+    <script src="assets/js/bootstrap.bundle.js"></script>
+    <script src="assets/js/loaders.css.js"></script>
+    <script src="assets/js/aos.js"></script>
+    <script src="assets/js/swiper.min.js"></script>
+    <script src="assets/js/lightgallery-all.min.js"></script>
+    <!--Template Script-->
+    <script src="assets/js/main.js"></script>
+</body>
+
+</html>
