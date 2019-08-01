55e4b5447e98783ae53ffb914298b4aafb948fce
Karake Omar <anzymen@gmail.com>
2019-08-01 17:39:11 +0200

    work for today

---
+++ domain.php
@@ -0,0 +1,88 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!--
+    /   Multipurpose: Free Template by FreeHTML5.co
+    /   Author: https://freehtml5.co
+    /   Facebook: https://facebook.com/fh5co
+    /   Twitter: https://twitter.com/fh5co
+    -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Document title -->
+    <title>Hosting system</title>
+    <!-- Stylesheets & Fonts -->
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,700i%7CRajdhani:400,600,700"
+        rel="stylesheet">
+    <!-- Plugins Stylesheets -->
+    <link rel="stylesheet" href="assets/css/loader/loaders.css">
+    <link rel="stylesheet" href="assets/css/font-awesome/font-awesome.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/aos/aos.css">
+    <link rel="stylesheet" href="assets/css/swiper/swiper.css">
+    <link rel="stylesheet" href="assets/css/lightgallery.min.css">
+    <!-- Template Stylesheet -->
+    <link rel="stylesheet" href="assets/css/style.css">
+    <!-- Responsive Stylesheet -->
+    <link rel="stylesheet" href="assets/css/responsive.css">
+</head>
+
+<body>
+    <!-- Loader Start -->
+    <div class="css-loader">
+        <div class="loader-inner line-scale d-flex align-items-center justify-content-center"></div>
+    </div>
+    <!-- Loader End -->
+    <!-- Header Start -->
+    <header class="position-absolute w-100">
+        <?php
+		include'navigation.php';
+		?>
+    </header>
+    <!-- Header End -->
+    <!-- Hero Start -->
+    <!-- Hero End -->
+    <!-- Call To Action Start -->
+    <!-- Call To Action End -->
+    <!-- Services Start -->
+    <!-- Services End -->
+    <!-- Featured Start -->
+    <!-- Featured End -->
+    <!-- Recent Posts Start -->
+    <!-- Recent Posts End -->
+    <!-- Trust Start -->
+    <!-- Trust End -->
+    <!-- Pricing Start -->
+    <section class="pricing-table">
+        <div class="container">
+            <div class="title text-center">
+                <h6 class="title-primary">         -</h6>
+                <h1 class="title-blue">Domains</h1>
+            </div>
+			<div class="row no-gutters">
+                <div class="col-md-12">
+                    <img src="domain1.png" class="img-rounded" alt="Domain names" width="100%" height="55%">
+                </div>
+            </div>
+        </div>
+    </section>
+    <?php
+	include'footer.php';
+	?>
+    <!-- Footer Endt -->
+    <!--jQuery-->
+    <script src="assets/js/jquery-3.3.1.js"></script>
+    <!--Plugins-->
+    <script src="assets/js/bootstrap.bundle.js"></script>
+    <script src="assets/js/loaders.css.js"></script>
+    <script src="assets/js/aos.js"></script>
+    <script src="assets/js/swiper.min.js"></script>
+    <script src="assets/js/lightgallery-all.min.js"></script>
+    <!--Template Script-->
+    <script src="assets/js/main.js"></script>
+</body>
+
+</html>
